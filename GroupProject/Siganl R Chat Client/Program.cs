59966f9a68e4672845018363b6312517704bbdc9
Hman1994 <al-hardiman@hotmail.com>
2016-01-18 19:21:40 +0000

    chat commit

--- GroupProject/Siganl R Chat Client/Program.cs
+++ GroupProject/Siganl R Chat Client/Program.cs
@@ -19,8 +19,22 @@ namespace Siganl_R_Chat_Client
             HubConnection connection = new HubConnection("http://localhost:52037");
             proxy = connection.CreateHubProxy("ChatHub");
 
-
-
         }
+
+            private static void Connection_Received(string obj)
+        {
+            Console.WriteLine("Message Recieved {0}", obj);
+        }
+
+        private static void recieved_a_message(string sender, string message)
+        {
+            Console.WriteLine("{0} : {1}", sender, message);
+        }
+
+        Action<string, string> SendMessageRecieved = recieved_a_message;
+
+
+
     }
 }
+
