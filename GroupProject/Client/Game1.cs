3b9aab63eb93b589706fd93e4686dc24b6853ba4
Hman1994 <al-hardiman@hotmail.com>
2016-01-19 23:38:32 +0000

    ..

--- GroupProject/Client/Game1.cs
+++ GroupProject/Client/Game1.cs
@@ -71,15 +71,10 @@ namespace Client
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+    
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            
             graphics.PreferredBackBufferHeight = 768;
             graphics.PreferredBackBufferWidth = 1280;
             graphics.ApplyChanges();
@@ -98,10 +93,7 @@ namespace Client
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+       
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -146,11 +138,6 @@ namespace Client
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             newState = Keyboard.GetState();
@@ -259,10 +246,7 @@ namespace Client
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
